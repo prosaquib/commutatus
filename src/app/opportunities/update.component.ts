349e664fea875367644714677192ac953cee8df6
Saquib Akhter <prosaquib.akhter@gmail.com>
2017-10-09 02:16:31 +0530

    Editing features

--- src/app/opportunities/update.component.ts
+++ src/app/opportunities/update.component.ts
@@ -41,7 +41,7 @@ export class OpportunityUpdateComponent implements OnInit {
 
         let tempSkill = Object.keys(this.opportunity.skills).map(key => this.opportunity.skills[key]);
         let tempBackground = Object.keys(this.opportunity.backgrounds).map(key => this.opportunity.backgrounds[key]);
-        console.log(tempSkill);
+        // console.log(tempSkill);
         tempSkill.forEach( t => {
           this.skill.push(t);
         });
@@ -50,7 +50,7 @@ export class OpportunityUpdateComponent implements OnInit {
           this.background.push(t);
         });
         this.skillservice.getSkills().subscribe(datas => {
-          console.log(datas);
+          // console.log(datas);
           
           datas.forEach(data => {
             this.skill.push(data);
@@ -67,43 +67,61 @@ export class OpportunityUpdateComponent implements OnInit {
   }
 
   onUpdate(val: any) {
-    console.log(val);
-    val.skills.option = "required";
-    val.skills.level = 0;
-    let data = val.title;
+    // console.log(val);
+    
+    // val.skills.option = "required";
+    // val.skills.level = 0;
+    // let data = val.title;
     let tempData = { opportunity:{}}
-    if(val.title){
-      console.log("welcome1");
+    if(val.title){      
+      tempData.opportunity = val;      
+    } if(val.application_close_date){      
       tempData.opportunity = val;
-    }else if(val.application_close_date){
-      console.log("welcome2");
+    } if(val.earliest_start_date){
       tempData.opportunity = val;
-    }else if(val.earliest_start_date){
+    } if(val.latest_end_date){
       tempData.opportunity = val;
-    }else if(val.latest_end_date){
+    } if(val.description){
       tempData.opportunity = val;
-    }else if(val.description){
+    } if(val.description){
       tempData.opportunity = val;
-    }else if(val.description){
-      tempData.opportunity = val;
-    }else if(val.skills){
-      console.log("welcome");
-      val.skills.option = "required";
-      val.skills.level = 0;
+    } if(val.skills){
+      console.log(val.skills);      
+      // let tempSkills = {
+      //   skills:[{
+      //   "option":"required",
+      //   "level": 0,
+      //   "id":val.skills[0].id,
+      //   "name":val.skills[0].name
+      //   }]
+      // }      
+      // console.log(tempSkills);
+
+      val.skills[0].option="required";
+      val.skills[0].level=0;
+      
+      // tempData.opportunity = tempSkills;
+      tempData.opportunity = val.skills;
       console.log(val.skills);
-      tempData.opportunity = val;
+      
+    } if(val.selection_process){
+      console.log("Hello");
+      let selectionProcess = {role_info:{"selection_process":val.selection_process}}
+      tempData.opportunity = selectionProcess;
     }
+    console.log(val);
+    
     this.opservice.updateOpportunity(tempData).subscribe(data => {
       this.result.class = 'alert alert-success';
       this.result.message = 'Success!Opportunity has been updated successfully.';
-      setTimeout(() => { this.result.class = ""; this.result.message = ""; }, 4000);
-      setTimeout(() => { this.router.navigate(['/opportunity-view']) }, 4000)
+      setTimeout(() => { this.result.class = ""; this.result.message = ""; }, 2000);
+      setTimeout(() => { this.router.navigate(['/opportunity-view']) }, 2000)
     },error=>{
       console.log(error.json().error)
       this.result.class = 'alert alert-warning';
       this.result.message = "Error!"+error.json().error;
-      setTimeout(() => { this.result.class = ""; this.result.message = ""; }, 4000);
-      setTimeout(() => { this.router.navigate(['/opportunity-view']) }, 4000)
+      setTimeout(() => { this.result.class = ""; this.result.message = ""; }, 2000);
+      setTimeout(() => { this.router.navigate(['/opportunity-view']) }, 2000)
     }
   );
   }
