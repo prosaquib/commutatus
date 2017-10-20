135d84a634a63c94534c2b38f09316bf2651c745
Saquib Akhter <prosaquib.akhter@gmail.com>
2017-10-20 12:29:30 +0530

    model has been changed

--- src/app/_models/opportunities.model.ts
+++ src/app/_models/opportunities.model.ts
@@ -1,19 +1,19 @@
-import {Skills} from './skills.model';
-import {RoleInfo} from './role_info.model';
-import {SpecificsInfo} from './specifics_info.model'
-import {Backgrounds} from './backgrounds.model'
-export class Opportunity{
+import { Skills } from './skills.model';
+import { RoleInfo } from './role_info.model';
+import { SpecificsInfo } from './specifics_info.model'
+import { Backgrounds } from './backgrounds.model'
+export class Opportunity {
     constructor(
-        id?:string,
-        title?:string,
-        applications_close_date?:string,
-        earliest_start_date?:string,
-        latest_end_date?:string,
-        description?:string,        
+        public id?: string,
+        public title?: string,
+        public applications_close_date?: string,
+        public earliest_start_date?: string,
+        public latest_end_date?: string,
+        public description?: string,
         public skills = new Skills(),
-        public role_info = new RoleInfo(),        
-        public specifics_info = new SpecificsInfo(),   
-        public backgrounds = new Backgrounds()             
-    ){}
+        public role_info = new RoleInfo(),
+        public specifics_info = new SpecificsInfo(),
+        public backgrounds = new Backgrounds()
+    ) { }
 }
 
